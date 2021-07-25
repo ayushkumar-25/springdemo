928cd6c3000e6946f8feacd64c45af913b583c5f
Ayush Kumar <ayush2608@gmail.com>
2021-07-25 19:01:28 +0530

    Update Tyre.java

--- src/main/java/com/example/telusko/Tyre.java
+++ src/main/java/com/example/telusko/Tyre.java
@@ -1,4 +1,21 @@
 package com.example.telusko;
 
 public class Tyre {
+
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Tyre{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
 }
